b158ed322f1c8d181f8c732251b74770193349a6
Nalishaa <nalishashakya@gmail.com>
2026-03-05 14:56:08 +0545

    Add item feature

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,24 @@ import Items from "./components/Items";
 import { groceryItems } from "./data/groceryItems";
 import { useState } from "react";
 import { ToastContainer, toast } from "react-toastify";
+import { nanoid } from "nanoid";
+import Form from "./components/Form";
 import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 
 const App = () => {
   const [items, setItems] = useState(groceryItems);
+
+  const addItem = (itemName) => {
+    const newItem = {
+      name: itemName,
+      completed: false,
+      id: nanoid(),
+    };
+    const newItems = [...items, newItem];
+    setItems(newItems);
+    toast.success("grocery item added");
+  };
 
   const editCompleted = (itemId) => {
     const newItems = items.map((item) => {
@@ -17,6 +30,7 @@ const App = () => {
     });
     setItems(newItems);
   };
+
   const removeItem = (itemId) => {
     const newItems = items.filter((item) => item.id !== itemId);
     setItems(newItems);
@@ -26,6 +40,7 @@ const App = () => {
   return (
     <section className="section-center">
       <ToastContainer position="top-center" />
+      <Form addItem={addItem} />
       <Items
         items={items}
         editCompleted={editCompleted}
